e42189ff3cbc5c1c03d4687ab69ea5589483785a
evatf01 <evatf01@gmail.com>
2022-01-24 19:40:12 +0100

    .

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyHomePage extends StatefulWidget {
   _State createState() => _State();
 }
 
-String strInput = "";
 final txtEntrada = TextEditingController();
 final txtResultado = TextEditingController();
 
@@ -31,7 +30,7 @@ class _State extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color.fromARGB(250, 184, 183, 181),
+        backgroundColor: const Color.fromARGB(250, 100, 100, 100),
         appBar: AppBar(title: const Text("Calculadora")),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -41,14 +40,14 @@ class _State extends State<MyHomePage> {
               child: TextField(
                 decoration: const InputDecoration.collapsed(
                     hintText: "0.0",
-                    focusColor: Colors.black,
+                    focusColor: Colors.white,
                     hintStyle:
                         TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
-                    fillColor: Colors.black),
+                    fillColor: Colors.white),
                 style: const TextStyle(
                     fontSize: 40.0,
                     fontWeight: FontWeight.bold,
-                    color: Colors.black),
+                    color: Colors.white),
                 textAlign: TextAlign.right,
                 controller: txtEntrada,
                 onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
@@ -59,23 +58,23 @@ class _State extends State<MyHomePage> {
                 child: TextField(
                   decoration: const InputDecoration.collapsed(
                       hintText: "",
-                      fillColor: Colors.black,
+                      fillColor: Colors.white,
                       hintStyle: TextStyle(
                           fontSize: 40.0, fontWeight: FontWeight.bold),
-                      focusColor: Colors.black),
+                      focusColor: Colors.white),
                   style: const TextStyle(
                       fontSize: 40.0,
-                      color: Colors.black,
+                      color: Colors.white,
                       fontWeight: FontWeight.bold),
                   textAlign: TextAlign.right,
                   controller: txtResultado,
                 )),
-            Container(height: 60.0),
+            Container(height: 70.0),
             Row(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                btnAC('AC', const Color.fromARGB(250, 169, 200, 250)),
+                btnAC('AC', const Color.fromARGB(250, 100, 100, 100)),
                 btnBorrar(),
                 boton('%'),
                 boton('/'),
@@ -126,16 +125,16 @@ class _State extends State<MyHomePage> {
 
   Widget boton(texto) {
     return Container(
-      padding: const EdgeInsets.only(bottom: 10.0),
+      padding: const EdgeInsets.only(bottom: 8.0),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(3.0),
         child: ElevatedButton(
           child: Text(
             texto,
             style: const TextStyle(
-                fontSize: 25.0,
+                fontSize: 27.0,
                 fontWeight: FontWeight.bold,
-                color: Colors.black),
+                color: Colors.white),
           ),
           onPressed: () {
             setState(() {
@@ -143,14 +142,14 @@ class _State extends State<MyHomePage> {
             });
           },
           style: ButtonStyle(
-            minimumSize: MaterialStateProperty.all(const Size(78.0, 78.0)),
+            minimumSize: MaterialStateProperty.all(const Size(85.0, 85.0)),
             backgroundColor: MaterialStateProperty.all<Color>(
-                const Color.fromARGB(250, 167, 223, 251)),
+                const Color.fromARGB(250, 100, 100, 100)),
             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30.0),
                   side: const BorderSide(
-                      width: 2.0, color: Color.fromARGB(200, 51, 91, 132))),
+                      width: 3.5, color: Color.fromARGB(200, 108, 207, 235))),
             ),
           ),
         ),
@@ -160,16 +159,16 @@ class _State extends State<MyHomePage> {
 
   Widget btnAC(texto, Color boton) {
     return Container(
-      padding: const EdgeInsets.only(bottom: 10.0),
+      padding: const EdgeInsets.only(bottom: 8.0),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(3.0),
         child: ElevatedButton(
           child: Text(
             texto,
             style: const TextStyle(
                 fontSize: 25.0,
                 fontWeight: FontWeight.bold,
-                color: Colors.black),
+                color: Colors.white),
           ),
           onPressed: () {
             setState(() {
@@ -178,14 +177,14 @@ class _State extends State<MyHomePage> {
             });
           },
           style: ButtonStyle(
-            minimumSize: MaterialStateProperty.all(const Size(78.0, 78.0)),
+            minimumSize: MaterialStateProperty.all(const Size(85.0, 85.0)),
             backgroundColor: MaterialStateProperty.all<Color>(
-                const Color.fromARGB(250, 167, 223, 251)),
+                const Color.fromARGB(250, 100, 100, 100)),
             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30.0),
                   side: const BorderSide(
-                      width: 2.0, color: Color.fromARGB(200, 51, 91, 132))),
+                      width: 3.5, color: Color.fromARGB(200, 108, 207, 235))),
             ),
           ),
         ),
@@ -195,25 +194,25 @@ class _State extends State<MyHomePage> {
 
   Widget btnBorrar() {
     return Container(
-      padding: const EdgeInsets.only(bottom: 10.0),
+      padding: const EdgeInsets.only(bottom: 8.0),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(3.0),
         child: ElevatedButton(
-          child: const Icon(Icons.backspace, size: 30, color: Colors.black),
+          child: const Icon(Icons.backspace, size: 30, color: Colors.white),
           onPressed: () {
             txtEntrada.text = (txtEntrada.text.isNotEmpty)
                 ? (txtEntrada.text.substring(0, txtEntrada.text.length - 1))
                 : "";
           },
           style: ButtonStyle(
-            minimumSize: MaterialStateProperty.all(const Size(78.0, 78.0)),
+            minimumSize: MaterialStateProperty.all(const Size(85.0, 85.0)),
             backgroundColor: MaterialStateProperty.all<Color>(
-                const Color.fromARGB(250, 167, 223, 251)),
+                const Color.fromARGB(250, 100, 100, 100)),
             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(30.0),
                   side: const BorderSide(
-                      width: 2.0, color: Color.fromARGB(200, 51, 91, 132))),
+                      width: 3.5, color: Color.fromARGB(200, 108, 207, 235))),
             ),
           ),
         ),
@@ -223,15 +222,15 @@ class _State extends State<MyHomePage> {
 
   Widget btnIgual() {
     return Container(
-      padding: const EdgeInsets.only(bottom: 10.0),
+      padding: const EdgeInsets.only(bottom: 8.0),
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(5.0),
         child: ElevatedButton(
           child: const Text(
             '=',
             style: TextStyle(
-                fontSize: 28.0,
-                color: Colors.black,
+                fontSize: 35.0,
+                color: Colors.white,
                 fontWeight: FontWeight.bold),
           ),
           onPressed: () {
@@ -240,14 +239,14 @@ class _State extends State<MyHomePage> {
             });
           },
           style: ButtonStyle(
-            minimumSize: MaterialStateProperty.all(const Size(100.0, 78.0)),
+            minimumSize: MaterialStateProperty.all(const Size(155.0, 85.0)),
             backgroundColor: MaterialStateProperty.all<Color>(
-                const Color.fromARGB(250, 167, 223, 251)),
+                const Color.fromARGB(250, 100, 100, 100)),
             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
               RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(25.0),
                   side: const BorderSide(
-                      width: 2.0, color: Color.fromARGB(200, 51, 91, 132))),
+                      width: 3.5, color: Color.fromARGB(200, 108, 207, 235))),
             ),
           ),
         ),
